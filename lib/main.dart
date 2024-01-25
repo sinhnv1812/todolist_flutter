0a7fc05cba44b9b946bce7c2f7ec02b04aad33d2
Nguyen Van Sinh <nguyenvansinh1812@gmail.com>
2024-01-25 15:58:14 +0700

    abc

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+void main(List<String> args) {
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: MyApp(),
+  ));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('ToDoList'),
+      ),
+    );
+  }
+}
